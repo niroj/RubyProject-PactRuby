a09323789414dcafebf02bb7b96aa3d8235a6a5f
pratuat <pratuat@gmail.com>
2011-11-30 22:09:25 +0545

    Developer class implemented

--- Developer.rb
+++ Developer.rb
@@ -2,8 +2,20 @@ require 'C:\Users\User\Desktop\RubyProject\Employee.rb'
 
 class Developer < Employee
 
-	attr_accessor :projects
+	attr_accessor :project
 	
 	def initialize(name,project)
 		super(name)
-		@project
+		self.project = project
+	end
+	
+	def display_all
+		super
+		puts self.project
+	end
+	
+end
+
+pratuat = Developer.new("pratuat","Blackberry BizcardArmy")
+puts pratuat.display_all
+		
